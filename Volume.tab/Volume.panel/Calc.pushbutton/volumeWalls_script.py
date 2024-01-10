74501985ffb261f2ad0e937a986b8d290652b775
jrfm <moesch.jonas@protonmail.com>
2024-01-10 12:42:28 +0100

    Delete unnessacary code

--- Volume.tab/Volume.panel/Calc.pushbutton/volumeWalls_script.py
+++ Volume.tab/Volume.panel/Calc.pushbutton/volumeWalls_script.py
@@ -1,44 +1,14 @@
 # -*- coding: utf-8 -*-
+
+"""This script calculates the volume of all walls in the project and prints the result in the PyRevit console."""
+
+__title__ = 'Total\nVolume'
+__author__ = 'Jonas Mösch'
+
 from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
-# these commands get executed in the current scope
-# of each new shell (but not for canned commands)
-#pylint: disable=all
-import clr
-clr.AddReferenceByPartialName('PresentationCore')
-clr.AddReferenceByPartialName('AdWindows')
-clr.AddReferenceByPartialName("PresentationFramework")
-clr.AddReferenceByPartialName('System')
-clr.AddReferenceByPartialName('System.Windows.Forms')
 
-from Autodesk.Revit import DB
-from Autodesk.Revit import UI
+doc = __revit__.ActiveUIDocument.Document
 
-import Autodesk.Windows as aw
-
-# creates variables for selected elements in global scope
-# e1, e2, ...
-max_elements = 5
-gdict = globals()
-uiapp = __revit__
-uidoc = uiapp.ActiveUIDocument
-if uidoc:
-    doc = uiapp.ActiveUIDocument.Document
-    selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
-    for idx, el in enumerate(selection):
-        if idx < max_elements:
-            gdict['e{}'.format(idx+1)] = el
-        else:
-            break
-
-# alert function
-def alert(msg):
-    TaskDialog.Show('RPS', msg)
-
-# quit function
-def quit():
-    __window__.Close()
-
-    
 # Create collector instance and collect all walls in the project
 # 1 Pass the current document you want to search to the collector
 # FilteredElementCollector(doc)
